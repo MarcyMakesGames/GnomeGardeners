84065ac2794de5e8670fcffa5510567631a09eb1
Ace Derrick Young <luadengames@gmail.com>
2021-04-03 11:20:50 +0200

    Basic Weather
    Basic weather system implemented; WeatherChanged event available to subscribe to, interpretation of weather effects need to be implemented on the affected object through WeatherType enum interpretation.

--- Assets/Scripts/Controllers/WeatherController.cs
+++ Assets/Scripts/Controllers/WeatherController.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class WeatherController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] protected List<WeatherObject> weatherTypes;
+    [SerializeField] protected float weatherDuration;
+
+    protected WeatherObject currentWeather;
+    protected float currentWeatherTimer;
+
+    public delegate void OnWeatherChange();
+    public event OnWeatherChange WeatherChanged;
+
+    protected void Start()
     {
-        
+        foreach(WeatherObject weather in weatherTypes)
+        {
+            if (weather.weatherType == WeatherType.Sunny)
+            {
+                currentWeather = weather;
+                AnnounceWeather();
+                break;
+            }
+        }
+
+        WeatherChanged += AnnounceWeather;
+        currentWeatherTimer = 0f;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected void Update()
     {
-        
+        WeatherCountdown();
     }
+
+    protected void AnnounceWeather()
+    {
+        Debug.Log("Current weather is: " + currentWeather.weatherType.ToString());
+    }
+
+    protected void WeatherCountdown()
+    {
+        currentWeatherTimer += Time.deltaTime;
+
+        if(currentWeatherTimer >= weatherDuration)
+        {
+            int selectedWeather = Random.Range(0, weatherTypes.Count);
+            currentWeather = weatherTypes[selectedWeather];
+
+            WeatherChanged();
+            currentWeatherTimer = 0f;
+        }
+    }
+}
+
+public enum WeatherType
+{
+    Sunny = 1,
+    Rainy = 2,
+    Windy = 3
 }
