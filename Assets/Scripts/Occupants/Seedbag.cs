7c1372f6b1693689d9abc400283d553ba9db7270
Paul Brandstetter <paulbrandstetter@tuta.io>
2021-05-16 17:48:54 +0200

    Plant ObjectPooling fixes

--- Assets/Scripts/Occupants/Seedbag.cs
+++ Assets/Scripts/Occupants/Seedbag.cs
@@ -6,16 +6,22 @@ namespace GnomeGardeners
 {
     public class Seedbag : Occupant
     {
-        public Seed[] dispensables = new Seed[3];
+        public bool onlySunflowerSeed = true;
+
+        public Seed sunflowerSeed;
+
+        public Seed[] allSeeds = new Seed[3];
 
         #region Unity Methods
 
         private new void Start()
         {
             base.Start();
-            dispensables[0] = new Seed(PoolKey.Plant_Sunflower);
-            dispensables[1] = new Seed(PoolKey.Plant_Peppermint);
-            dispensables[2] = new Seed(PoolKey.Plant_Strawberry);
+            allSeeds[0] = new Seed(PoolKey.Plant_Sunflower);
+            allSeeds[1] = new Seed(PoolKey.Plant_Peppermint);
+            allSeeds[2] = new Seed(PoolKey.Plant_Strawberry);
+
+            sunflowerSeed = new Seed(PoolKey.Plant_Sunflower);
         }
 
         #endregion
@@ -29,10 +35,17 @@ namespace GnomeGardeners
         public Seed GetRandomDispensable()
         {
             GameManager.Instance.AudioManager.PlaySound(SoundType.sfx_seedbag_dispense, GetComponent<AudioSource>());
-            var randomIndex = Random.Range(0, dispensables.Length);
-            var randomSeed = dispensables[randomIndex];
+            var randomIndex = Random.Range(0, allSeeds.Length);
+            var randomSeed = allSeeds[randomIndex];
             DebugLogger.Log(this, "Dispensing." + randomSeed.ToString());
             return randomSeed;
+        }
+
+        public Seed GetSunflowerSeed()
+        {
+            GameManager.Instance.AudioManager.PlaySound(SoundType.sfx_seedbag_dispense, GetComponent<AudioSource>());
+            DebugLogger.Log(this, "Dispensing." + sunflowerSeed.ToString());
+            return sunflowerSeed;
         }
 
         #endregion
