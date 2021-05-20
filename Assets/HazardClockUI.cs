b86f2a761f4ada9ff65b4d0b3523f4e399a2514d
Paul Brandstetter <paulbrandstetter@tuta.io>
2021-05-20 16:39:12 +0200

    Hazard Clock
    Added HazardClockUI controlling the hazard clock gameObject inside the UI
    Added HazardEventChannel with OnNextHazard
    Changed HazardManager to know which hazard comes next

--- Assets/HazardClockUI.cs
+++ Assets/HazardClockUI.cs
@@ -31,6 +31,7 @@ namespace GnomeGardeners
             var gameObjectToSpawn = new GameObject("Icon");
             var image = gameObjectToSpawn.AddComponent<Image>();
             image.sprite = icon;
+            image.SetNativeSize();
             var spawnedGO = Instantiate(gameObjectToSpawn, posRight.position, gameObjectToSpawn.transform.rotation, mask.transform);
             StartCoroutine(MoveAcross(spawnedGO, delay, duration));
         }
@@ -40,16 +41,16 @@ namespace GnomeGardeners
             float elapsedTime = 0;
             while (elapsedTime < delay)
             {
-                transform.position = Vector3.Lerp(posRight.position, posCenter.position, (elapsedTime / delay));
+                objectToMove.transform.position = Vector3.Lerp(posRight.position, posCenter.position, (elapsedTime / delay));
                 elapsedTime += Time.deltaTime;
                 yield return new WaitForEndOfFrame();
             }
-            transform.position = posCenter.position;
+            objectToMove.transform.position = posCenter.position;
 
             elapsedTime = 0;
-            while (elapsedTime < delay)
+            while (elapsedTime < duration)
             {
-                transform.position = Vector3.Lerp(posCenter.position, posLeft.position, (elapsedTime / delay));
+                objectToMove.transform.position = Vector3.Lerp(posCenter.position, posLeft.position, (elapsedTime / duration));
                 elapsedTime += Time.deltaTime;
                 yield return new WaitForEndOfFrame();
             }
