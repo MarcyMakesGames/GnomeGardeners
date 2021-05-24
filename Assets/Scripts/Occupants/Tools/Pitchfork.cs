80691406b5acf34e30c2b3492e0f3a18f07f6a0e
Paul Brandstetter <paulbrandstetter@tuta.io>
2021-05-24 18:28:07 +0200

    Pitchfork Implementation
    Added pitchfork prefab
    Added Pitchfork class inheriting from tool

---
+++ Assets/Scripts/Occupants/Tools/Pitchfork.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.U2D.Animation;
+
+namespace GnomeGardeners
+{
+	public class Pitchfork : Tool
+	{
+		private Fertilizer fertilizer;
+
+		public override void Interact(Tool tool)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override void FailedInteraction()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override void UseTool(GridCell cell)
+		{
+			var occupant = cell.Occupant;
+			if (occupant != null)
+			{
+				Plant plant;
+				if (occupant.TryGetComponent(out plant) && fertilizer != null)
+				{
+					DebugLogger.Log(this, "Plant found while carrying Fertilizer!");
+					plant.FulfillCurrentNeed(NeedType.Fertilizer);
+					fertilizer = null;
+					return;
+				}
+				
+				Compost compost;
+				if (occupant.TryGetComponent(out compost))
+				{
+					if (fertilizer == null)
+					{
+						DebugLogger.Log(this, "Taking fertilizer.");
+						fertilizer = compost.DispenseItem();
+						return;
+					}
+					else if (fertilizer != null)
+					{
+						DebugLogger.Log(this, "Discarding fertilizer.");
+						fertilizer = null;
+						return;
+					}
+				}
+				
+				occupant.FailedInteraction();
+			}
+		}
+
+		public override void UpdateSpriteResolvers(SpriteResolver[] resolvers)
+		{
+			
+			foreach (SpriteResolver resolver in resolvers)
+			{
+				resolver.SetCategoryAndLabel("tools", "compost");
+			}
+		}
+	}
+}
